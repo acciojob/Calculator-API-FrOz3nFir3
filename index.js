e1704b7087be9fbffeec641695452b0ae2234455
Kush Surti <30488380+FrOz3nFir3@users.noreply.github.com>
2022-12-27 16:14:08 +0530

    added post routes

--- index.js
+++ index.js
@@ -9,8 +9,91 @@ app.use(express.static(__dirname))
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/main.html'));
+  res.send("Hello world!");
 });
 
+
+app.post("/add", (req, res)=>{
+  const {num1, num2} = req.body;
+
+  let status = "success"
+  let message = "The sum of given two numbers"
+  let result = num1 + num2;
+
+  if(typeof num1 == "string" || typeof num2 == "string"){
+    status = 'error';
+    message = 'Invalid data types';
+  } else if(result > 1000000 || num1 > 1000000 || num2 > 1000000){
+    status = "error";
+    message ="Overflow";
+  }else if(result < -1000000 || num1 < -1000000 || num2 < -1000000){
+    status = 'error';
+    message = 'Underflow';
+  }
+
+  res.json({status, message, result})
+})
+app.post("/sub", (req, res)=>{
+  const {num1, num2} = req.body;
+
+  let status = "success"
+  let message = "The difference of given two numbers"
+  let result = num1 - num2;
+
+  if(typeof num1 == "string" || typeof num2 == "string"){
+    status = 'error';
+    message = 'Invalid data types';
+  } else if(result > 1000000 || num1 > 1000000 || num2 > 1000000){
+    status = "error";
+    message ="Overflow";
+  }else if(result < -1000000 || num1 < -1000000 || num2 < -1000000){
+    status = 'error';
+    message = 'Underflow';
+  }
+
+  res.json({status, message, result})
+})
+app.post("/multiply", (req, res)=>{
+  const {num1, num2} = req.body;
+
+  let status = "success"
+  let message = "The product of given two numbers"
+  let result = num1 * num2;
+
+  if(typeof num1 == "string" || typeof num2 == "string"){
+    status = 'error';
+    message = 'Invalid data types';
+  } else if(result > 1000000 || num1 > 1000000 || num2 > 1000000){
+    status = "error";
+    message ="Overflow";
+  }else if(result < -1000000 || num1 < -1000000 || num2 < -1000000){
+    status = 'error';
+    message = 'Underflow';
+  }
+  res.json({status, message, result})
+})
+app.post("/divide", (req, res)=>{
+  const {num1, num2} = req.body;
+
+  let status = "success"
+  let message = "The division of given two numbers"
+  let result = num1 / num2;
+
+  if (num2 == 0){
+    status = 'error';
+    message = 'Cannot divide by zero'
+  }else if(typeof num1 == "string" || typeof num2 == "string"){
+    status = 'error';
+    message = 'Invalid data types';
+  } else if(result > 1000000 || num1 > 1000000 || num2 > 1000000){
+    status = "error";
+    message ="Overflow";
+  }else if(result < -1000000 || num1 < -1000000 || num2 < -1000000){
+    status = 'error';
+    message = 'Underflow';
+  }
+
+  res.json({status, message, result})
+})
 //your code here
 module.exports = app;
